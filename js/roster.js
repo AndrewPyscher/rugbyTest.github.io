e7e6f81ee8292d0fe835d5695ae1d545828b0126
AndrewPyscher <andypyscher@gmail.com>
2022-06-12 20:32:36 -0400

    rosterCarrds

--- js/roster.js
+++ js/roster.js
@@ -2,9 +2,11 @@ $('.element').each(function() {
   $(this).mouseover(function() {
       $(this).addClass('active');
     $('.stage').children('.element').not('.active').addClass('inactive');
+    console.log("here")
   });
   $(this).mouseleave(function() {
       $(this).removeClass('active');
       $('.stage').children('.element').not('.active').removeClass('inactive');
+      console.log("gone")
   });
 });
