8332c7880b527b6c7969f77ac51089f7822d3f3a
AndrewPyscher <andypyscher@gmail.com>
2022-06-14 22:39:44 -0400

    Update roster.js

--- js/roster.js
+++ js/roster.js
@@ -1,4 +1,8 @@
 
+//                        from database
+// make images in format "(playerNumber)(Name).png"
+// so we can splice where its not a number anymore to return to front when done hovering
+
   const container = document.querySelector('.stage')
 
   for(let i=1; i<4; i++){
