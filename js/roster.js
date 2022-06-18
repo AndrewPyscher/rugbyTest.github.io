d2ac47f95bb8632880acd34ebd53fd28e7e31247
AndrewPyscher <andypyscher@gmail.com>
2022-06-17 23:28:51 -0400

    yes

--- js/roster.js
+++ js/roster.js
@@ -1,42 +1,64 @@
 
-//                        from database
-// make images in format "(playerNumber)(Name).png"
-// so we can splice where its not a number anymore to return to front when done hovering
+// //                        from database
+// // make images in format "(playerNumber)(Name).png"
+// // so we can splice where its not a number anymore to return to front when done hovering
 
-  const container = document.querySelector('.stage')
+  const spot = document.querySelector('.spot');
+  console.log(spot)
+  for(let i=1; i<10; i++){ 
+    const flipCard = document.createElement('div')
+    flipCard.classList.add('flip-card');
 
-  for(let i=1; i<10; i++){
-    const playerDisplay = document.createElement('div');
-    playerDisplay.classList.add('element');
-    playerDisplay.classList.add('zero');
-    playerDisplay.style.background=`url('../js/player${(i%3) +1}.png') 45%`
+    const inner = document.createElement('div')
+    inner.classList.add('flip-card-inner')
 
-    playerDisplay.style.backgroundSize ="cover";
+    const front = document.createElement('div')
+    front.classList.add('flip-card-front')
+
+    const back = document.createElement('div')
+    back.classList.add('flip-card-back')
+
+
+
+    const newImgF = document.createElement('img');
+    newImgF.src = "../assets/front.png";
+    newImgF.classList.add('image1')
   
-    container.appendChild(playerDisplay);
-
-  }
+    const newImgP = document.createElement('img');
+    newImgP.src = "../assets/player1.png";
+    newImgP.classList.add('image2');
   
-let background = null;
-$('.element').each(function() {
-  $(this).mouseover(function() {
-      $(this).addClass('active');
-    $('.stage').children('.element').not('.active').addClass('inactive');
+    const logo = document.createElement('img');
+    logo.src = "../assets/logo.png";
+    logo.classList.add('logo');
+  
+    const position = document.createElement('h3');
+    // position.textContent = "IDK Positions";
+    position.classList.add('positionOfPlayer');
+  
+    const name = document.createElement('h2');
+    // name.textContent = 'Kanye West';
+    name.classList.add('nameOfPlayer')
 
-    if(background!= $(this)[0].style.background){
-      console.log(background)
-    background = $(this)[0].style.background;
-    }
-    $(this)[0].style.background="url('../js/sickmonkeyBack.jpeg') 45%"
-    $(this)[0].style.backgroundSize="cover"
+    front.appendChild(newImgF);
+    front.appendChild(newImgP);
+    front.appendChild(logo);
+    front.appendChild(position);
+    front.appendChild(name);
 
-  });
-  $(this).mouseleave(function() {
-      $(this).removeClass('active');
-      $('.stage').children('.element').not('.active').removeClass('inactive');
-      $(this)[0].style.background=`${background}`
-      console.log(background + " leave")
-      $(this)[0].style.backgroundSize="cover"
-  });
-});
+    inner.appendChild(front);
 
+
+    const p = document.createElement('p');
+    p.innerHTML = 'Kanye West';
+    
+    back.appendChild(p)
+
+    inner.appendChild(front);
+    inner.appendChild(back);
+
+    flipCard.appendChild(inner);
+    spot.appendChild(flipCard)
+
+
+}
