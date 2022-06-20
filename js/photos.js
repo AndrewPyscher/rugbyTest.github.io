aa0c714b1085fecde6c3e7ceac684d10df8b7628
AndrewPyscher <andypyscher@gmail.com>
2022-06-19 22:12:25 -0400

    make photos show up

--- js/photos.js
+++ js/photos.js
@@ -1,14 +1,14 @@
-// const container = document.querySelector('.row')
+const container = document.querySelector('.row')
 
-// for(let i=1; i<10; i++){
-//   const row = document.createElement('div');
-//   row.classList.add('col-xl-4');
-//   row.classList.add('gx-5')
-//   row.classList.add('gy-5')
-//   const img = document.createElement('img');
-//   img.src = `../css/rugby${(i%3) + 1}.png`
+for(let i=1; i<10; i++){
+  const row = document.createElement('div');
+  row.classList.add('col-xl-4');
+  row.classList.add('gx-5')
+  row.classList.add('gy-5')
+  const img = document.createElement('img');
+  img.src = `../css/rugby${(i%3) + 1}.png`
   
-//   row.appendChild(img);
-//   container.appendChild(row);
-//   console.log("here")
-// }
+  row.appendChild(img);
+  container.appendChild(row);
+  console.log("here")
+}
