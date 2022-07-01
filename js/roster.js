deff7e0ac5e950ce2e3b90e16f719c03a5828034
AndrewPyscher <andypyscher@gmail.com>
2022-06-30 21:58:18 -0400

    0

--- js/roster.js
+++ js/roster.js
@@ -1,28 +1,75 @@
 
-players = []
+array = []
 
 
 
-for(let i=0; i<10; i++){ 
-  let player ={
-    firstName: 'Kanye',
-    lastName: 'West',
-    position : 'IDK Positions',
-    age : 21,
-    height : "5'6",
-    year : 'sophomore',
-    major : 'Computer Science',
+
+let player ={
+    firstName: 'Jasmine',
+    lastName: 'Day',
+    position : 'Coach',
+    age : 23,
+    height : "5'7",
+    year : 'Coach',
+    major : 'People Person',
     hometown : 'Saginaw'
 }
+array.push(player)
 
-players.push(player)
-
+let player1 ={
+  firstName: 'Steve',
+  lastName: 'Jobs',
+  position : 'Graveyard',
+  age : 60,
+  height : "6 feet under",
+  year : 'dead',
+  major : 'iPhone',
+  hometown : 'The dirt'
 }
+
+let player2 ={
+  firstName: 'Flynn',
+  lastName: 'Cat',
+  position : 'Window',
+  age : 6,
+  height : "1'",
+  year : 'cat',
+  major : 'sleeping',
+  hometown : 'home'
+}
+let player3 ={
+  firstName: 'Kanye',
+  lastName: 'West',
+  position : 'God Lover',
+  age : 45,
+  height : "6'",
+  year : 'who knows',
+  major : 'sleeping',
+  hometown : 'God'
+}
+
+let player4 ={
+  firstName: 'Liv',
+  lastName: 'Coach',
+  position : 'Coach',
+  age : 22,
+  height : "5'5",
+  year : 'idk',
+  major : 'idk',
+  hometown : 'idk'
+}
+
+
+array.push(player1)
+array.push(player2)
+array.push(player3)
+array.push(player4)
+
 
 
   const spot = document.querySelector('.spotPlayers');
   console.log(spot)
-  for(let i=0; i<10; i++){ 
+  for(let i=0; i<array.length; i++){ 
     const flipCard = document.createElement('div')
     flipCard.classList.add('flip-card');
 
@@ -42,24 +89,23 @@ players.push(player)
     newImgF.classList.add('image1')
   
     const newImgP = document.createElement('img');
-    newImgP.src = `../assets/${players[i].firstName}${players[i].lastName}.png`;
+    newImgP.src = `../assets/${array[i].firstName}${array[i].lastName}.png`;
     newImgP.classList.add('image2');
   
     const logo = document.createElement('img');
-    logo.src = "../assets/logo.png";
+    logo.src = "../assets/test.png";
     logo.classList.add('logo');
   
     const position = document.createElement('h3');
-    position.textContent = `${players[i].position}`;
+    position.textContent = `${array[i].position}`;
     position.classList.add('positionOfPlayer');
   
     const name = document.createElement('h2');
-    name.textContent = `${players[i].firstName} ${players[i].lastName}`;
+    name.textContent = `${array[i].firstName} ${array[i].lastName}`;
     name.classList.add('nameOfPlayer')
 
     front.appendChild(newImgF);
     front.appendChild(newImgP);
-    front.appendChild(logo);
     front.appendChild(position);
     front.appendChild(name);
 
@@ -72,7 +118,7 @@ players.push(player)
     back.appendChild(newImgB)
 
     const p = document.createElement('p');
-    p.innerHTML = `Name: ${players[i].firstName} ${players[i].lastName} <br><br>Age: ${players[i].age}<br><br>Height: ${players[i].height}<br><br>Class: ${players[i].year}<br><br>Position: ${players[i].position}<br><br>Major: ${players[i].major}<br><br>Hometown: ${players[i].hometown}`;
+    p.innerHTML = `<b>Name: ${array[i].firstName} ${array[i].lastName} <br><br>Age: ${array[i].age}<br><br>Height: ${array[i].height}<br><br>Class: ${array[i].year}<br><br>Position: ${array[i].position}<br><br>Major: ${array[i].major}<br><br>Hometown: ${array[i].hometown}</b>`;
     
     back.appendChild(p)
 
@@ -88,7 +134,7 @@ players.push(player)
 
 const spotC = document.querySelector('.spotCoaches');
   console.log(spot)
-  for(let i=0; i<5; i++){ 
+  for(let i=0; i<array.length; i++){ 
     const flipCard = document.createElement('div')
     flipCard.classList.add('flip-card');
 
@@ -108,7 +154,7 @@ const spotC = document.querySelector('.spotCoaches');
     newImgF.classList.add('image1')
   
     const newImgP = document.createElement('img');
-    newImgP.src = `../assets/${players[i].firstName}${players[i].lastName}.png`;
+    newImgP.src = `../assets/${array[i].firstName}${array[i].lastName}.png`;
     newImgP.classList.add('image2');
   
     const logo = document.createElement('img');
@@ -116,16 +162,15 @@ const spotC = document.querySelector('.spotCoaches');
     logo.classList.add('logo');
   
     const position = document.createElement('h3');
-    position.textContent = `${players[i].position}`;
+    position.textContent = `${array[i].position}`;
     position.classList.add('positionOfPlayer');
   
     const name = document.createElement('h2');
-    name.textContent = `${players[i].firstName} ${players[i].lastName}`;
+    name.textContent = `${array[i].firstName} ${array[i].lastName}`;
     name.classList.add('nameOfPlayer')
 
     front.appendChild(newImgF);
     front.appendChild(newImgP);
-    front.appendChild(logo);
     front.appendChild(position);
     front.appendChild(name);
 
@@ -138,7 +183,7 @@ const spotC = document.querySelector('.spotCoaches');
     back.appendChild(newImgB)
 
     const p = document.createElement('p');
-    p.innerHTML = `Name: ${players[i].firstName} ${players[i].lastName} <br><br>Age: ${players[i].age}<br><br>Height: ${players[i].height}<br><br>Year: ${players[i].year}<br><br>Position: ${players[i].position}<br><br>Major: ${players[i].major}<br><br>Hometown: ${players[i].hometown}`;
+    p.innerHTML = `Name: ${array[i].firstName} ${array[i].lastName} <br><br>Age: ${array[i].age}<br><br>Height: ${array[i].height}<br><br>Year: ${array[i].year}<br><br>Position: ${array[i].position}<br><br>Major: ${array[i].major}<br><br>Hometown: ${array[i].hometown}`;
     
     back.appendChild(p)
 
